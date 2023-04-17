0e07aec4c47bff36d2d7d1fe0a7949664334157f
anyelot <81717749+anyelot@users.noreply.github.com>
2023-04-17 11:16:06 -0500

    prueba unitaria (distancia)
    aqui se tiene la prueba unitaria para medir la distancia entre dos puntos

--- test/in_action_test.dart
+++ test/in_action_test.dart
@@ -1,1 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:geolocator/geolocator.dart';
 
+void main() {
+  group('Geolocation Tests', () {
+    test('Calculate distance between two points', () async {
+      final position1 = Position(
+        latitude: 37.7749,
+        longitude: -122.4194,
+        altitude: 50.0,
+        speed: 10.0,
+        speedAccuracy: 1.0,
+        heading: 90.0,
+        accuracy: 5.0,
+        timestamp: DateTime.now(),
+      );
+
+      final position2 = Position(
+        latitude: 40.7128,
+        longitude: -74.0060,
+        altitude: 50.0,
+        speed: 10.0,
+        speedAccuracy: 1.0,
+        heading: 90.0,
+        accuracy: 5.0,
+        timestamp: DateTime.now(),
+      );
+
+      final distance = await Geolocator.distanceBetween(
+        position1.latitude,
+        position1.longitude,
+        position2.latitude,
+        position2.longitude,
+      );
+
+      expect(distance, closeTo(4139.87, 20000));
+    });
+  });
+}
